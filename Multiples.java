a253d31156636fa8788f8ef7b49f36f6fb92a33f
Humachine <swarun@gmail.com>
2016-05-21 20:08:12 -0700

    Beating 122 with readability

--- Multiples.java
+++ Multiples.java
@@ -1,1 +1,5 @@
-public class Multiples{public static void main(String args[]){for(int i=0,j=0;j<100;i+=3,j++)System.out.println(i);}}
+public class Multiples{
+	public static void main(String args[]){
+		for(int j=0;j<300;j+=3)
+			System.out.println(j);
+}}
